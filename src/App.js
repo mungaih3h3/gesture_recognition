7572becec027487d732abe74d111b495d7181654
Brian Mungai <mungaihaha@gmail.com>
2022-01-10 07:19:42 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -1,47 +1,30 @@
-// 0. Install fingerpose npm install fingerpose
-// 1. Add Use State
-// 2. Import emojis and finger pose import * as fp from "fingerpose";
-// 3. Setup hook and emoji object
-// 4. Update detect function for gesture handling
-// 5. Add emoji display to the screen
-
-///////// NEW STUFF ADDED USE STATE
 import React, { useRef, useState, useEffect } from "react";
-///////// NEW STUFF ADDED USE STATE
 
-// import logo from './logo.svg';
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 import "./App.css";
 import { drawHand } from "./utilities";
 
-///////// NEW STUFF IMPORTS
 import * as fp from "fingerpose";
 import victory from "./victory.png";
 import thumbs_up from "./thumbs_up.png";
-///////// NEW STUFF IMPORTS
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
-
-  ///////// NEW STUFF ADDED STATE HOOK
   const [emoji, setEmoji] = useState(null);
   const images = { thumbs_up: thumbs_up, victory: victory };
-  ///////// NEW STUFF ADDED STATE HOOK
 
   const runHandpose = async () => {
     const net = await handpose.load();
     console.log("Handpose model loaded.");
-    //  Loop and detect hands
     setInterval(() => {
       detect(net);
     }, 10);
   };
 
   const detect = async (net) => {
-    // Check data is available
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
@@ -62,10 +45,6 @@ function App() {
 
       // Make Detections
       const hand = await net.estimateHands(video);
-      // console.log(hand);
-
-      ///////// NEW STUFF ADDED GESTURE HANDLING
-
       if (hand.length > 0) {
         const GE = new fp.GestureEstimator([
           fp.Gestures.VictoryGesture,
@@ -86,16 +65,14 @@ function App() {
           console.log(emoji);
         }
       }
-
-      ///////// NEW STUFF ADDED GESTURE HANDLING
-
-      // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
       drawHand(hand, ctx);
     }
   };
 
-  useEffect(()=>{runHandpose()},[]);
+  useEffect(() => {
+    runHandpose();
+  }, []);
 
   return (
     <div className="App">
@@ -147,8 +124,6 @@ function App() {
         ) : (
           ""
         )}
-
-        {/* NEW STUFF */}
       </header>
     </div>
   );
